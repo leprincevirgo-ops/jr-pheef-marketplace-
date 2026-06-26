6dec643f7a9812131385d035dfce8897721565b1
Le Prince Virgo <leprincevirgo@gmail.com>
2026-06-26 09:04:06 +0300

    Add Supabase client initialization

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET_KEY
+);
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
